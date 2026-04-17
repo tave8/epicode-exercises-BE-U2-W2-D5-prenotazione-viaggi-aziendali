638289a4180445eedc81e74ca39504bdfdfd7956
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-17 08:45:07 +0200

    Initial commit

---
+++ src/test/java/giuseppetavella/D5/D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppetavella.D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
